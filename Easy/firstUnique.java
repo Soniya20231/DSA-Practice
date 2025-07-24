6a2f073c3756d0fab2ac5691e037e49a1fb97430
Soniya Pawar <soniya000pawar@gmail.com>
2025-07-24 12:15:00 +0530

    first non repeating char in string

---
+++ Easy/firstUnique.java
@@ -0,0 +1,14 @@
+// First Unique Character in a String
+// Given a string s, find the first non-repeating character and return its
+// index. If it does not exist, return -1.
+// Eg. Input: "loveleetcode"
+// Output: 2 (The first unique character is 'v'.)
+
+package Easy;
+
+public class firstUnique {
+    
+    public static void main(String[] args) {
+        String st="loveleetcode";
+    }
+}
